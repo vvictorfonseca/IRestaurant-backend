659b9fadad236f9246489d34e82ed2e976f41932
matheusfacciolla <matheustassifacciolla@gmail.com>
2023-05-26 00:40:09 -0300

    refactor: add service and repository patterns

--- src/resolvers/signin-resolver.ts
+++ src/resolvers/signin-resolver.ts
@@ -1,54 +1,18 @@
 import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
-import { ApolloError } from "apollo-server";
 
-import { Context, context } from "../context";
+import { Context } from "../context";
 import { CreateSigninInput } from "../dtos/inputs/signin-user-input";
 import { SiginUser } from "../dtos/models/sigin-user-model";
+import { SigninService } from "../services/signin-service";
 
-import bcrypt from 'bcrypt';
-import jwt from "jsonwebtoken";
+const signinService = new SigninService()
 
 @Resolver()
 export class SigninResolver {
 
   @Mutation(() => SiginUser)
-  async signIn(@Arg('data') data: CreateSigninInput) {
-
-    const userInfo = await context.prisma.users.findFirst({
-      where: {
-        email: data.email
-      },
-      select: {
-        id: true,
-        email: true,
-        password: true,
-        firstName: true,
-        lastName: true
-      }
-    });
-
-    if (!userInfo) {
-      throw new ApolloError('E-mail not register', 'NOT_FOUND');
-    }
-
-    const isCorrectPassword = bcrypt.compareSync(
-      data.password,
-      userInfo.password
-    );
-
-    if (!isCorrectPassword) {
-      throw new ApolloError('Wrong password', 'UNAUTHORIZED');
-    }
-
-    const key: string = (process.env.JWT_SECRET as string);
-    const expiresAt = { expiresIn: 60 * 60 * 24 };
-    const token = jwt.sign(
-      { id: userInfo.id, email: userInfo.email, name: userInfo.lastName },
-      key,
-      expiresAt
-    );
-
-    const res = {...userInfo, token: token};
-    return res;
+  async signIn(@Arg('data') data: CreateSigninInput, @Ctx() ctx: Context): Promise<SiginUser> {
+    
+    return signinService.findUserByEmail(data, ctx);
   }
 }
